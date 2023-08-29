3ca62616afc8cd6e7a21ada09f71f5cc2101f250
RubenYoo <rubenchiche0317@gmail.com>
2023-08-29 10:32:24 +0300

    some fixes and typing

--- notification/notification.py
+++ notification/notification.py
@@ -8,14 +8,25 @@ TWILIO_ACCOUNT_SID: Final[str] = os.getenv('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN: Final[str] = os.getenv('TWILIO_AUTH_TOKEN')
 FROM_NUMBER: Final[str] = os.getenv('FROM_NUMBER')
 
-client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-
 
 class NotificationManager:
+    """
+    This class is responsible for sending notifications.
+    """
+
+    client: Client
+
     def __int__(self):
         self.client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
-    def notify_user(self, to_number, message_to_send):
+    def notify_user(self, to_number: str, message_to_send: str) -> None:
+        """
+        Sends a message to the user
+        :param to_number: number to send
+        :param message_to_send: message to send
+        :return: None
+        """
+
         message = self.client.messages \
             .create(
                  body=message_to_send,
@@ -24,3 +35,8 @@ class NotificationManager:
              )
 
         print(message.sid)
+
+    def ask_doors(self, doors) -> None:
+        # TODO
+        pass
+
